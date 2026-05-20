b5c616e18081ab0526c102c0c55ad65f60c8965d
Tim-Chou <cyt.twtim810512@gmail.com>
2026-05-20 14:17:14 +0800

    finish an origin bash44 shell version 0.0.1

---
+++ bash44_patches.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+numbers=12
+num=1
+
+while [ $num -le $numbers ]
+do
+  if [ $num -lt 10 ]; then
+     apatch="patch -p0 < ../bash44-00$num"
+     echo "$apatch"
+     num=$(( $num+1 ))
+  else
+     apatch="patch -p0 < ../bash4-0$num"
+     echo "$apatch"
+     num=$(( $num+1))
+  fi
+done
